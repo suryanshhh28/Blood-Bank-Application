0b5a2cdf00fdec28e66a775089c05ae294cea8c9
Suryansh Chaudhary <suryanshchaudhary28@gmail.com>
2023-07-04 22:04:11 +0530

    Initial folders committed

---
+++ routes/testRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const { testController } = require("../controller/testController")
+
+const router = express.Router()
+
+router.get('/', testController)
+
+module.exports = router
